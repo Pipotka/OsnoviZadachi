839a7f21ba9b488db5513e7a32728cabae7ee811
Nasrudin <33nasrudin@gmail.com>
2024-09-07 17:00:42 +0300

    initial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+fun main(args: Array<String>)
+{
+    print("Введите строку: ");
+    var str = readln();
+    var result = "";
+    var countChar = 1
+    var previousChar = str [0]
+    for (i in 1 .. str.length - 1)
+    {
+        if (previousChar == str[i])
+        {
+            countChar +=1
+            previousChar =str[i]
+        }
+        else
+        {
+            if (countChar > 1)
+            {
+                result = result + countChar + previousChar
+            }
+            else
+            {
+                result = result + previousChar
+            }
+            countChar = 1
+            previousChar = str[i]
+        }
+    }
+    if ( countChar > 1)
+    {
+        result = result + countChar + previousChar
+
+    }
+    else
+    {
+        result = result + previousChar
+    }
+    print(str+countChar);
+
+}
+
